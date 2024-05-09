cac36123e39b3f70c1cb173f45071660142ee8d3
Carles Serna <carles.serna@gmail.com>
2024-05-09 12:37:28 +0200

    fix styles

--- Joma/src/pages/CatalogoPage.jsx
+++ Joma/src/pages/CatalogoPage.jsx
@@ -7,7 +7,6 @@ import KitCard from "../components/KitCard";
 import Tabs from "../components/Tabs";
 
 function CatalogoPage() {
-  const [selectedValue, setSelectedValue] = useState("");
   const [categories, setCategories] = useState([]);
   const [featuredProducts, setFeaturedProducts] = useState([]);
   const [menuItems, setMenu] = useState([]);
@@ -34,34 +33,18 @@ function CatalogoPage() {
     fetchData();
   }, []);
 
-  const handleSelectChange = (event) => {
-    console.log("Selected value: ", event.target.value);
-    setSelectedValue(event.target.value);
-  };
-
   return (
     <>
       <section className="max-w-screen-xl mx-auto p-4 md:p-8 lg:p-12">
         <ProductCarouselComponent />
-        <div className="flex justify-between mt-10">
+        <div className="mt-10">
           <div>
             <label htmlFor="hs-select-label" className="block  font-bold mb-2">
               Categorías
             </label>
-            <select
-              id="hs-select-label"
-              className="py-3 px-4 pe-9 block  border-2 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-              onChange={handleSelectChange}
-            >
-              {categories.map((category) => (
-                <option key={category.id} value={category.attributes.name}>
-                  {category.attributes.name}
-                </option>
-              ))}
-            </select>
+            <Tabs menuItems={menuItems} />
           </div>
         </div>
-        <Tabs menuItems={menuItems} />
         <div className="grid grid-cols-4 mt-10 gap-5">
           <h3 className="col-span-4 text-center mb-4">PRODUCTOS DESTACADOS</h3>
           {featuredProducts.map((featuredItem) => (
@@ -72,23 +55,6 @@ function CatalogoPage() {
             />
           ))}
         </div>
-        {/* <div className="grid grid-cols-4 mt-10 gap-5">
-          {selectedCategory ? (
-            selectedCategory.subcategory.map((subcat) => (
-              <KitCard key={subcat} title={subcat} />
-            ))
-          ) : (
-            <>
-              <h3 className="col-span-4 text-center mb-4">
-                PRODUCTOS DESTACADOS
-              </h3>
-              <KitCard title="Test 1" />
-              <KitCard title="Test 2" />
-              <KitCard title="Test 3" />
-              <KitCard title="Test 4" />
-            </>
-          )}
-        </div> */}
       </section>
     </>
   );
