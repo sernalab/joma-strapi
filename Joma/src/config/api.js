269bda6f4246ac3ffaa84830b281e2248ac12bf5
Carles Serna <carles.serna@gmail.com>
2024-05-14 19:16:27 +0200

    add new endpoint to show products by category

--- Joma/src/config/api.js
+++ Joma/src/config/api.js
@@ -14,6 +14,36 @@ export async function getJobs() {
   }
 }
 
+export async function getProducts() {
+  try {
+    const response = await fetch(`${API_URL}/products`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const { data } = await response.json();
+    console.log("Products", data);
+    return data;
+  } catch (error) {
+    console.log("Error al cargar las vacantes:", error);
+    return [];
+  }
+}
+
+export async function getProductsByCategory(id_category) {
+  try {
+    const response = await fetch(
+      `${API_URL}/products?populate=category&filters[id_category][$eq]=${id_category}&pagination[pageSize]=100`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const { data } = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Error fetching products by category:", error);
+    return [];
+  }
+}
 export async function getNewProducts() {
   try {
     const response = await fetch(`${API_URL}/new-products`);
@@ -60,7 +90,7 @@ export async function getCategories() {
 
 export async function getMenu() {
   try {
-    const response = await fetch(`${API_URL}/menus`);
+    const response = await fetch(`${API_URL}/menus?populate=categories`);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
--- Joma/src/pages/CatalogoPage.jsx
+++ Joma/src/pages/CatalogoPage.jsx
@@ -1,30 +1,34 @@
 import { useState, useEffect } from "react";
+import {
+  getCategories,
+  getMenu,
+  getProducts,
+  getProductsByCategory,
+} from "../config/api";
 
-import { getCategories, getMenu, getFeaturedProducts } from "../config/api";
-
+import ProductsMenu from "../components/Ecommerce/ProductsMenu";
 import ProductCarouselComponent from "../components/ProductCarousel";
-import KitCard from "../components/KitCard";
-import Tabs from "../components/Tabs";
+import Tabs from "../components/Ecommerce/Tabs";
+import FeaturedProducts from "../components/Ecommerce/FeaturedProducts";
 
 function CatalogoPage() {
-  const [categories, setCategories] = useState([]);
-  const [featuredProducts, setFeaturedProducts] = useState([]);
+  const [categoriesData, setCategories] = useState([]);
+  const [productsData, setProducts] = useState([]);
+  const [selectedCategoryProducts, setSelectedCategoryProducts] = useState([]);
   const [menuItems, setMenu] = useState([]);
+  const [selectedMenu, setSelectedMenu] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       try {
         const categoriesData = await getCategories();
         setCategories(categoriesData);
-        console.log("categories", categories);
 
         const menuItems = await getMenu();
         setMenu(menuItems);
-        console.log("menuItems", menuItems);
 
-        const featuredData = await getFeaturedProducts();
-        setFeaturedProducts(featuredData);
-        console.log(featuredData);
+        const productsData = await getProducts();
+        setProducts(productsData);
       } catch (error) {
         console.log("ERROR API CATEGORIES");
       }
@@ -33,28 +37,75 @@ function CatalogoPage() {
     fetchData();
   }, []);
 
+  const handleChange = (event) => {
+    setSelectedMenu(event.target.value);
+  };
+
+  const handleCategoryClick = async (id_category) => {
+    try {
+      const products = await getProductsByCategory(id_category);
+      setSelectedCategoryProducts(products);
+    } catch (error) {
+      console.log("ERROR FETCHING PRODUCTS BY CATEGORY", error);
+    }
+  };
+
+  const selectedMenuItem = menuItems.find(
+    (menu) => menu.id.toString() === selectedMenu
+  );
+
   return (
     <>
       <section className="max-w-screen-xl mx-auto p-4 md:p-8 lg:p-12">
         <ProductCarouselComponent />
+
         <div className="mt-10">
-          <div>
-            <label htmlFor="hs-select-label" className="block  font-bold mb-2">
-              Categorías
-            </label>
-            <Tabs menuItems={menuItems} />
+          <span className="font-bold">Categorías</span>
+          <select
+            name="categories"
+            className="mb-10"
+            value={selectedMenu}
+            onChange={handleChange}
+          >
+            <option value="" disabled>
+              - Seleccione un menú -
+            </option>
+            {menuItems.map((menuItem) => (
+              <option key={menuItem.id} value={menuItem.id}>
+                {menuItem.attributes.title}(
+                {menuItem.attributes.categories.data.length} categorías)
+              </option>
+            ))}
+          </select>
+          {selectedMenuItem && (
+            <ul>
+              {selectedMenuItem.attributes.categories.data.map((category) => (
+                <li key={category.id}>
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleCategoryClick(category.attributes.id_category);
+                    }}
+                  >
+                    {category.attributes.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        {selectedCategoryProducts.length > 0 && (
+          <div className="mt-10">
+            <h2 className="font-bold">Productos</h2>
+            <ul>
+              {selectedCategoryProducts.map((product) => (
+                <li key={product.id}>{product.attributes.name}</li>
+              ))}
+            </ul>
           </div>
-        </div>
-        <div className="grid grid-cols-4 mt-10 gap-5">
-          <h3 className="col-span-4 text-center mb-4">PRODUCTOS DESTACADOS</h3>
-          {featuredProducts.map((featuredItem) => (
-            <KitCard
-              key={featuredItem.id}
-              title={featuredItem.attributes.title}
-              shortDescription={featuredItem.attributes.shortDescription}
-            />
-          ))}
-        </div>
+        )}
+        <FeaturedProducts />
       </section>
     </>
   );
